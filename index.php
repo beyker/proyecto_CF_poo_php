16ca9692d1c392711a7ac5231f7cd3d71ff6eb6b
Beyker Monterrey <beykerpreteroti07@gmail.com>
2018-09-24 16:07:10 -0400

    cambio de comentarios

--- index.php
+++ index.php
@@ -2,18 +2,6 @@
 // ini_set('display_errors', 1);
 // error_reporting(E_ALL);
 
-
-
-//constantes
-//define('DS', DIRECTORY_SEPARATOR); 
-//define('ROOT', realpath(dirname(__FILE__)) . DS);
-
-//require_once "Config/Autoload.php";
-//Config\Autoload::run();
-//new Models\Estudiante();
-//new Config\Request();
-
-
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', realpath(dirname(__FILE__)) . DS);
 	define('URL', "http://localhost/proyecto/");
